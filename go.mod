102655eec9e3ab95e0f0ba587ddf1d3e355421ad
rollrat <rollrat.cse@gmail.com>
2022-11-26 10:28:19 +0900

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module scs-packet-capturer
 
 go 1.18
 
-require github.com/ghedo/go.pkt v0.0.0-20200209120728-c97f47ad982f // indirect
+require (
+	github.com/ghedo/go.pkt v0.0.0-20200209120728-c97f47ad982f // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/ghedo/go.pkt/capture/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
 func main() {
@@ -30,6 +33,47 @@ func main() {
 		log.Println("PACKET!!!")
 
 		// do something with the packet
-		log.Println(buf)
+		packet := gopacket.NewPacket(buf, layers.LayerTypeIPv4, gopacket.Default)
+
+		// Get the TCP layer from this packet
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+			fmt.Println("This is a TCP packet!")
+			// Get actual TCP data from this layer
+			tcp, _ := tcpLayer.(*layers.TCP)
+			fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
+		}
+
+		if packet.ApplicationLayer() != nil {
+			body := packet.ApplicationLayer().Payload()
+			if len(body) > 0 {
+				bodyStr := string(body)
+
+				fmt.Println(bodyStr)
+
+				// modify payload of application layer
+				*packet.ApplicationLayer().(*gopacket.Payload) = []byte("GET / HTTP/1.1")
+
+				// if its tcp we need to tell it which network layer is being used
+				// to be able to handle multiple protocols we can add a if clause around this
+				packet.TransportLayer().(*layers.TCP).SetNetworkLayerForChecksum(packet.NetworkLayer())
+
+				buffer := gopacket.NewSerializeBuffer()
+				options := gopacket.SerializeOptions{
+					ComputeChecksums: true,
+					FixLengths:       true,
+				}
+
+				// Serialize Packet to get raw bytes
+				if err := gopacket.SerializePacket(buffer, options, packet); err != nil {
+					log.Fatalln(err)
+				}
+
+				packetBytes := buffer.Bytes()
+
+				fmt.Println(packetBytes)
+
+				fmt.Println("-- ")
+			}
+		}
 	}
 }
