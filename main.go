fa2440995e7b8a2a75e914df26930798780c6599
rollrat <rollrat.cse@gmail.com>
2022-11-26 10:56:35 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/ghedo/go.pkt/capture/pcap"
@@ -37,7 +36,7 @@ func main() {
 		}
 
 		if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
-			fmt.Println("This is a IP packet!")
+			log.Println("This is a IP packet!")
 			ip, _ := ipLayer.(*layers.IPv4)
 			log.Printf("From src ip %s to src ip %s\n", ip.SrcIP, ip.DstIP)
 		}
@@ -48,31 +47,32 @@ func main() {
 			log.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
 		}
 
-		// for _, layer := range packet.Layers() {
-		// 	log.Println("PACKET LAYER:", layer.LayerType())
-		// 	body := string(layer.LayerPayload())
-		// 	log.Println(body)
-		// }
-
-		if packet.ApplicationLayer() != nil {
-			body := packet.ApplicationLayer().Payload()
-			if len(body) > 0 {
-				buffer := gopacket.NewSerializeBuffer()
-				options := gopacket.SerializeOptions{
-					ComputeChecksums: true,
-					FixLengths:       true,
-				}
-
-				if err := gopacket.SerializePacket(buffer, options, packet); err != nil {
-					log.Fatalln(err)
-				}
-
-				packetBytes := buffer.Bytes()
-
-				fmt.Println(packetBytes)
-
-				fmt.Println("-- ")
-			}
+		for _, layer := range packet.Layers() {
+			log.Println("PACKET LAYER:", layer.LayerType())
+			// body := string(layer.LayerPayload())
+			// log.Println(body)
+			log.Println(gopacket.LayerDump(layer))
 		}
+
+		// if packet.ApplicationLayer() != nil {
+		// 	body := packet.ApplicationLayer().Payload()
+		// 	if len(body) > 0 {
+		// 		buffer := gopacket.NewSerializeBuffer()
+		// 		options := gopacket.SerializeOptions{
+		// 			ComputeChecksums: true,
+		// 			FixLengths:       true,
+		// 		}
+
+		// 		if err := gopacket.SerializePacket(buffer, options, packet); err != nil {
+		// 			log.Fatalln(err)
+		// 		}
+
+		// 		packetBytes := buffer.Bytes()
+
+		// 		fmt.Println(packetBytes)
+
+		// 		fmt.Println("-- ")
+		// 	}
+		// }
 	}
 }
